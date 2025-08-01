9cae80cc687f4d6bdf1b416b83d128e9027b1ceb
rhmpatan <abdulrhmpatangmail.com>
2025-08-01 16:14:47 +0530

    t

---
+++ src/test/java/seleniumsessions/practice/ReverseString.java
@@ -0,0 +1,14 @@
+package seleniumsessions.practice;
+
+public class ReverseString {
+	 public static void main(String[] args) {
+	        String input = "Raheem";
+	        String reversed = "";
+
+	        for (int i = input.length() - 1; i >= 0; i--) {
+	            reversed += input.charAt(i);
+	        }
+
+	        System.out.println("Reversed String: " + reversed);
+	    }
+}
